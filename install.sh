790b013a9f6a4beecfebd2ac7ad4eed08191399c
Nicholas Duffy <duffn@outlook.com>
2020-10-14 14:58:37 -0600

    Revert "Allow user to select MariaDB as database engine"

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ REMOTE="git@github.com:duffn/gatsby-headless-wordpress-starter.git"
 BRANCH="main"
 LOCAL="gatsby-headless-wordpress-starter"
 
-# The single option.
-MARIADB=${MARIADB:-no}
-
 command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
@@ -29,31 +26,13 @@ main() {
     exit 1
   fi
 
-  while [[ $# -gt 0 ]]; do
-    key="$1"
-
-    case $key in
-      --mariadb)
-        MARIADB=yes
-        shift
-        ;;
-      *)
-        ;;
-    esac
-  done
-
   git clone --depth=1 \
     --config transfer.fsckobjects=false \
     --config receive.fsckobjects=false \
     --config fetch.fsckobjects=false \
     --branch "$BRANCH" "$REMOTE" "$LOCAL"
   cd "$LOCAL"
-
-  if [[ "$MARIADB" == "yes" ]]; then
-    docker-compose -f docker-compose.yml -f docker-compose.mariadb.yml up --build
-  else
-    docker-compose -f docker-compose.yml -f docker-compose.mysql.yml up --build
-  fi
+  docker-compose up --build
 }
 
-main "$@"
+main
